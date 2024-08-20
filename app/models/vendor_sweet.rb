13904e095f641c3a8a47c20ecbe8a5382e411b2b
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-20 11:33:56 -0400

    Create validations for join table model

--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -1,4 +1,10 @@
 class VendorSweet < ApplicationRecord
   belongs_to :vendor
   belongs_to :sweet
+
+  validates :vendor_id, uniqueness: { scope: :sweet_id, message: "and sweet combination already exists" }
+  validates :vendor_id, presence: true
+  validates :sweet_id, presence: true
+  validates :comment, presence: true
+
 end
