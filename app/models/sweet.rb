c0af0c07e8e24acdc6b2d2965fa07bc4ea7871b6
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-20 11:34:25 -0400

    Create method to return comment on that vendor/sweet association

--- app/models/sweet.rb
+++ app/models/sweet.rb
@@ -1,4 +1,8 @@
 class Sweet < ApplicationRecord
   has_many :vendor_sweets
   has_many :vendors, through: :vendor_sweets
+
+  def sweet_comment(id)
+    comment = vendor_sweets.find_by(vendor_id: id, sweet_id: self.id)&.comment
+  end
 end
